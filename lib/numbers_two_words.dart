aef3c6812d5ee722af61f66ae36e986ce5f3b8f5
Khalid Durrani <khaliddurrani113@gmail.com>
2025-03-20 10:50:11 +0430

    sample localizationf for three language done pubspec.yaml file initialized such as name and description

--- lib/numbers_two_words.dart
+++ lib/numbers_two_words.dart
@@ -1,5 +1,31 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+library num_to_words;
+
+import './localization/english.dart';
+import './localization/pashto.dart';
+import './localization/dari.dart';
+
+class NumbersTwoWords {
+  static String getNumber(int no, String language) {
+    Map<int, String> data;
+
+    switch (language) {
+      case 'ps':
+        data = numbersTwoWordsPashto;
+        break;
+      case 'en':
+        data = numbersTwoWordsEnglish;
+        break;
+      case 'fa':
+        data = numbersTwoWordsDari;
+        break;
+      default:
+        return "Invalid Language";
+    }
+
+    if (data.containsKey(no)) {
+      return data[no]!;
+    } else {
+      return "Invalid Number";
+    }
+  }
 }
